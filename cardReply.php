f979b140538eaedb60e55fc9c0baf7ac8be747a6
katasu-git <katasu9923@gmail.com>
2020-07-21 21:18:25 +0900

    cardReplyを切り出し

---
+++ cardReply.php
@@ -0,0 +1,35 @@
+<?php
+
+function cardReply($rumors) {
+    $cardMessages = [];
+    $file = "./carousel.json";
+    $json = file_get_contents($file);
+    $array = json_decode($json, true);
+    for($i=0; $i<count($rumors); $i++) {
+        array_push(
+            $array["template"]["columns"],
+            [
+                "title"=> $rumors[$i]['fix'] . "人 が疑ってます",
+                "text"=> cutText($rumors[$i]['contents']),
+                "actions"=> [
+                  [
+                    "type"=> "uri",
+                    "label"=> "もっと詳しく！",
+                    "uri"=> $rumors[$i]['url']
+                  ]
+                ]
+            ]
+        );
+    }
+    array_push(
+        $cardMessages, $array
+    );
+    array_push(
+        $cardMessages, 
+        [
+            "type"=>"text",
+            "text"=>count($rumors) . "件の怪しい情報見つかったよ！\n" . "もっと詳しく！を押すと、訂正情報が見られるよ。"
+        ]
+    );
+    return $cardMessages;
+}
--- repFunctions.php
+++ repFunctions.php
@@ -1,5 +1,6 @@
 <?php
 require_once './rumor-background/RestAPI/writeLog.php';
+require_once './cardReply.php';
 # writeLog($userText, 0, $userId, $noMatch); //ユーザのメッセージ
 # テキストの内容，ボット=>1，ユーザID，インテント失敗=>1
 
@@ -79,40 +80,6 @@ function simpleReply($texts) {
         );
     }
     return $messages;
-}
-
-function cardReply($rumors) {
-    $cardMessages = [];
-    $file = "./carousel.json";
-    $json = file_get_contents($file);
-    $array = json_decode($json, true);
-    for($i=0; $i<count($rumors); $i++) {
-        array_push(
-            $array["template"]["columns"],
-            [
-                "title"=> $rumors[$i]['fix'] . "人 が疑ってます",
-                "text"=> cutText($rumors[$i]['contents']),
-                "actions"=> [
-                  [
-                    "type"=> "uri",
-                    "label"=> "もっと詳しく！",
-                    "uri"=> $rumors[$i]['url']
-                  ]
-                ]
-            ]
-        );
-    }
-    array_push(
-        $cardMessages, $array
-    );
-    array_push(
-        $cardMessages, 
-        [
-            "type"=>"text",
-            "text"=>count($rumors) . "件の怪しい情報見つかったよ！\n" . "もっと詳しく！を押すと、訂正情報が見られるよ。"
-        ]
-    );
-    return $cardMessages;
 }
 
 function writeRumorsLog($rumors) {
