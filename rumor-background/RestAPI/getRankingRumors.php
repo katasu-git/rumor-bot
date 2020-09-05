503c161c6f997714efd90e9cbf9a8434a1d44acb
katasu-git <katasu9923@gmail.com>
2020-09-05 11:53:41 +0900

    ランキングの参照範囲を一週間に修正

--- rumor-background/RestAPI/getRankingRumors.php
+++ rumor-background/RestAPI/getRankingRumors.php
@@ -4,8 +4,11 @@ require_once dirname(__FILE__) . "/../../functions/connect_mysql.php";
 require_once dirname(__FILE__) . "/../RestAPI/sortDesk.php";
 
 function getRankingRumors() {
+    $today = date("Y-m-d");
+    $weekday = date("Y-m-d",strtotime("-7 day"));
+
     $pdo = connectMysql();  //mysqlに接続
-    $sql = "SELECT * FROM rumors ORDER BY fix DESC LIMIT 5"; // シングルコート必要
+    $sql = "SELECT * FROM rumors WHERE created_at BETWEEN '$weekday' AND '$today' ORDER BY fix DESC LIMIT 5"; // シングルコート必要
     $stmt = $pdo -> query($sql) -> fetchAll();
     return $stmt;
 }
